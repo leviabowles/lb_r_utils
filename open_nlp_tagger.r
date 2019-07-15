6a3130c60377dcd6ad3ad22d559b857412fe06c3
leviabowles <48260571+leviabowles@users.noreply.github.com>
2019-07-15 13:24:09 -0500

    Create open_nlp_tagger.r

---
+++ open_nlp_tagger.r
@@ -0,0 +1,27 @@
+
+library(NLP)
+library(openNLP)
+## Some text.
+## FUNCTION TO RETURN A POS TABLE
+pos_table = function(x){
+  s = as.String(x)
+  #create annotators
+  sent_token_annotator = Maxent_Sent_Token_Annotator()
+  word_token_annotator = Maxent_Word_Token_Annotator()
+  
+  #first level annottation
+  a2 = annotate(s, list(sent_token_annotator, word_token_annotator))
+  
+  #POS ANNOTATION
+  pos_tag_annotator = Maxent_POS_Tag_Annotator()
+  a3 =annotate(s, pos_tag_annotator, a2)
+  
+  
+  
+  ##we just want words
+  a3w = subset(a3, type == "word")
+  #and the tags, in a vector
+  tags = sapply(a3w$features, `[[`, "POS")
+  #put it in a data frame
+  return(data.frame(word = s[a3w],features =  tags))
+}
