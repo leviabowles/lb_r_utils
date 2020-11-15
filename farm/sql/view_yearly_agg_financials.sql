1fcd0b5199ad48e6b4fbeed7cfd5a4f32a8f5690
leviabowles <48260571+leviabowles@users.noreply.github.com>
2020-11-14 21:36:01 -0600

    Update view_yearly_agg_financials.sql

--- farm/sql/view_yearly_agg_financials.sql
+++ farm/sql/view_yearly_agg_financials.sql
@@ -1,17 +1,26 @@
+drop view if exists yearly_agg_financials;
 create view yearly_agg_financials as
 
-select trans.field_key, 
-	trans.year_key,
+select fy.field_key, 
+	fy.year_key,
     trans.costs,
     trans.revenue,
     alp.interest_payment,
-    alp.principal_payment
-from 
-		(select fyt.field_key,
+    alp.principal_payment,
+    yy.capital_payment
+from field_year fy 
+	left join (select fyt.field_key,
 			fyt.year_key,
 			sum(fyt.paid_amount) as costs,
 			sum(fyt.received_amount) as revenue
 		from field_year_transaction fyt 
-		group by fyt.field_key, fyt.year_key) trans
-	left join  annual_loan_payment alp on alp.field_key = trans.field_key
-		and alp.year_key = trans.year_key
+		group by fyt.field_key, fyt.year_key) trans on trans.field_key = fy.field_key
+			and trans.year_key = fy.year_key
+	left join  annual_loan_payment alp on alp.field_key = fy.field_key
+		and alp.year_key = fy.year_key
+	left join (select cp.field_key, 
+			cp.year_key,
+			sum(cp.total_payment) as capital_payment
+        from capital_payment cp
+        group by cp.field_key, cp.year_key) yy on yy.field_key = fy.field_key
+			and yy.year_key = fy.year_key;
