40194c80f10e025785321143c57ebacc3263b8b3
leviabowles <48260571+leviabowles@users.noreply.github.com>
2020-11-29 20:45:54 -0600

    Update monty_hall_simulator.R

--- r_fun_utils/monty_hall_simulator.R
+++ r_fun_utils/monty_hall_simulator.R
@@ -1,16 +1,26 @@
-simulations = 1000000
-doors = 3
-switcher = 0
+## Monty Hall simulation game for variable number of games played, whether switch strategy is in play and variable numbers of doors
 
-sim_matrix = matrix(nrow = simulations, ncol = doors, runif(simulations*doors))
-winners = apply(sim_matrix, 1, which.max)
-door = c(1:doors)
-chosen = sample(door,simulations, replace = TRUE)
-catch = c()
-for(i in c(1:simulations)){
-    eliminate = sample(setdiff(door,c(chosen[i],winners[i])),1)
-    if(switcher == 0){chosed = chosen[i]}else{chosed = setdiff(door,c(eliminate,chosen[i]))}
-    result = if(chosed == winners[i]){1}else{0}
-    catch = c(result, catch)
+simulations = 10000
+doors = 5
+switcher = 1
+
+
+monty_hall = function(simulations,doors,switcher){
+    sim_matrix = matrix(nrow = simulations, ncol = doors, runif(simulations*doors))
+    winners = apply(sim_matrix, 1, which.max)
+    door = c(1:doors)
+    chosen = sample(door,simulations, replace = TRUE)
+    catch = c()
+    for(i in c(1:simulations)){
+        eliminate = sample(setdiff(door,c(chosen[i],winners[i])),1)
+        if(switcher == 0){chosed = chosen[i]}else{chosed = sample(setdiff(door,c(eliminate,chosen[i])),1)}
+        print(length(chosed))
+        print(chosed)
+        result = if(chosed == winners[i]){1}else{0}
+        catch = c(result, catch)
+    }
+    summary(catch)
 }
-summary(catch)
+
+monty_hall(simulations, doors, switcher)
+
