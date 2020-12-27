01af7cb1b830cac7a0e3e15829af8a3ffbb149fc
leviabowles <48260571+leviabowles@users.noreply.github.com>
2020-12-26 22:35:24 -0600

    Update wrapper.R

--- r_timeseries/wrapper.R
+++ r_timeseries/wrapper.R
@@ -1,18 +1,28 @@
+library(caret)
+wd = "/Users/lbowle11/Documents/GitHub/leviabowles_repo/r_timeseries"
+setwd(wd)
+source("lb_ts.R")
 wd = "/Users/lbowle11/Documents"
 setwd(wd)
 xx = read.csv("wheat.csv")
 xx$date = as.Date(xx$date, "%m/%d/%Y")
 row.names(xx) = xx$date
-ff$value_lag = my_lag(ff$value,1)
-ff$log_return = log(ff$value/ff$value_lag)
+
+
+xx = fill_daily(xx)
+xx$value_lag = my_lag(xx$value,1)
+xx$log_return = log_return(xx$value,xx$value_lag)
+xx = multi_lag(xx,"log_return",10)
+xx = add_seasonality(xx)
+
+xx = xx[complete.cases(xx),]
+xx = xx[,c(4:ncol(xx))]
+
+
+mod = caret::train(log_return~.,data = xx, type = "GBM")
 
 
 
-for(i in c(1:5)){
-  ff[,paste0("log_return_lag",as.character(i))] = my_lag(ff$log_return,i)
-}
-
-ff = ff[complete.cases(ff),]
 mod = earth(log_return ~ weekday + log_return_lag1 + log_return_lag2+ log_return_lag3+ log_return_lag4+ log_return_lag5+factor(month) , degree = 2,ff)
 
 
