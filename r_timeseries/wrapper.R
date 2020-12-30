13a992f5d1da242ed0a4471daf84c014a4856b96
leviabowles <48260571+leviabowles@users.noreply.github.com>
2020-12-29 22:02:03 -0600

    Update wrapper.R

--- r_timeseries/wrapper.R
+++ r_timeseries/wrapper.R
@@ -12,15 +12,10 @@ a = lb_ts(df = xx, dvar = "value")
 a = fill_daily(a)
 a = log_return_dvar(a)
 a = multi_lag(a, "value",10)
-
-xx$value_lag = my_lag(xx$value,1)
-xx$log_return = log_return(xx$value,xx$value_lag)
-xx = multi_lag(xx,"log_return",10)
-xx = add_seasonality(xx)
-
-xx = xx[complete.cases(xx),]
-
-dd = time_slice(xx,10)
+a = add_seasonality(a)
+a = complete_only(a)
+a = time_slice(a,increment = 100)
+a = time_build_models(a,c("lm"),iter = 20)
 
 jj = time_slice_validate(dd,c("lm"), iter = 50)
 summary(lm(predicted_value ~ real_value ,jj))
