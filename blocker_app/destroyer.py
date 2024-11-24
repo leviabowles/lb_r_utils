f4792579af6b22bdd50f490131b7a39ebbd9a77f
leviabowles <48260571+leviabowles@users.noreply.github.com>
2024-11-23 22:23:36 -0600

    Update destroyer.py

--- blocker_app/destroyer.py
+++ blocker_app/destroyer.py
@@ -4,3 +4,19 @@ import pandas as pd
 import os
 import warnings
 warnings.filterwarnings("ignore")
+
+
+api = tweepy.API(auth)
+
+
+
+df = pd.read_csv(fp, dtype={'id': str})
+listo = df['id'].to_list()
+
+for ID in listo[5:30]:
+    try: 
+        api.destroy_status(ID)
+        time.sleep(15)
+        print(ID)
+    except:
+        print(ID)
